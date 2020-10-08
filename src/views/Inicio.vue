73fe7883934db95a63d753e2cea2313565caab23
Museo <Miranda@Macbooks-MacBook-Pro.local>
2020-10-08 20:00:47 +0200

    1

---
+++ src/views/Inicio.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="inicio">
+    <Navbar></Navbar>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <div>
+        <b-button>Button</b-button>
+        <b-button variant="danger">Button</b-button>
+        <b-button variant="success">Button</b-button>
+        <b-button variant="outline-primary">Button</b-button>
+    </div>
+</div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+    name: 'Inicio',
+    components: {
+        Navbar
+    }
+}
+</script>
